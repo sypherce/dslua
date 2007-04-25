26e4ba7fab6248813010b29e8b94ea409c798353
jeremy.ruten <jeremy.ruten@7bd1a944-e22a-0410-b2d1-0368184b35f6>
2007-04-25 01:04:12 +0000

    nomsg

---
+++ trunk/DSLUA/source/DSLImage.h
@@ -0,0 +1,16 @@
+#ifndef _DSLIMAGE_H_
+#define _DSLIMAGE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lua.h"
+
+int luaopen_DSLImageLib(lua_State *);
+
+#ifdef __cplusplus
+}          // extern "C"
+#endif
+
+#endif // _DSLIMAGE_H_
